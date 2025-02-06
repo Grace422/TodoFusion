b5a02e0873556eb86c3d76f5d71f1d80af5d7a6e
Grace <neguekwahammaelgrace@gmail.com>
2025-02-06 13:19:31 +0100

    shadows added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,30 @@
 import Link from "next/link";
+import { Moon } from "lucide-react";
 const Navbar = () => {
-    return ( 
-        <div>
+  return (
+    <div className="h-full w-full mx-auto max-w-[1350px] px-4 py-6 md:px-20">
+      <nav className="mx-auto w-full h-full flex items-center gap-8 transition-all duration-200 rounded-full border border-transparent">
+        <div className="size-8 rounded-md transition-all duration-200 bg-slate-900" />
+        <div className="flex items-center justify-between ml-auto lg:flex-1 lg:ml-0">
+          <div className="hidden items-center gap-8 lg:flex">
             <Link href="/home">Home</Link>
             <Link href="/features">Features</Link>
             <Link href="/integrations">Integrations</Link>
             <Link href="/pricing">Pricing</Link>
             <Link href="/docs">Docs</Link>
             <Link href="/contacts">Contacts</Link>
+          </div>
+          <div className="flex items-center gap-3">
+            <button className="flex items-center justify-center border rounded-full border-transparent size-8">
+              <Moon size={15} />
+            </button>
+            <div className="h-8 w-[1px] bg-gray-500" />
+            <button className="bg-violet-800 rounded-md text-white px-6 py-2 shadow">Get Template</button>
+          </div>
         </div>
-     );
-}
- 
+      </nav>
+    </div>
+  );
+};
+
 export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
       <div className="h-full w-full mx-auto px-40 max-w-[1350px] md:px-20">
         <div className="flex flex-col items-center justify-center min-h-screen py-40 gap-32">
           <div className="max-w-2xl text-center flex flex-col gap-5">
-            <h1 className="font-semibold text-3xl">
+            <h1 className="font-semibold text-3xl lg:text-[56px] md:text-4xl lg:leading-[60px]">
               Boost Your Productivity with{" "}
-              <span className="text-primary">TodoFusion</span>
+              <span className="text-violet-800">TodoFusion</span>
             </h1>
             <p className="text-muted-foreground">
               Transform task management with TodoFusion. Combine powerful to-do
@@ -19,28 +19,34 @@ export default function Home() {
               focused and achieve more
             </p>
             <div className="flex gap-5 items-center justify-center">
-              <button>Get Started</button>
-              <button>See how it works</button>
+              <button className="px-6 py-2 bg-violet-700 rounded-md shadow">
+                Get Started
+              </button>
+              <button className="bg-slate-950 rounded-md px-6 py-2 shadow">
+                See how it works
+              </button>
             </div>
           </div>
 
-          <div className="border h-full rounded-3xl bg-background/50 backdrop-blur-lg p-5">
-            <Image
-              src="/set.webp"
-              alt="site picture"
-              width={900}
-              height={900}
-              objectFit="contain"
-            />
-          </div>
+            <div className="borders h-full rounded-3xl image-color backdrop-blur-lg p-6 shadow-[0px_0px_100px_5px_rgba(59,92,163,0.4)]">
+              <Image
+                src="/set.webp"
+                alt="site picture"
+                width={900}
+                height={900}
+                objectFit="contain"
+              />
+            </div>
         </div>
       </div>
 
-      <div className="h-full w-full mx-auto px-40 max-w-[1350px]">
-        <p className="w-full h-full">
-          Trusted by <span className="font-bold">800K+</span> freelancers and{" "}
-          <span className="font-bold">20k+ teams</span> like.
-        </p>
+      <div className="h-full w-full mx-auto px-4 md:px-20 text-center max-w-[1350px]">
+        <div className="w-full h-full">
+          <p className="mt-6">
+            Trusted by <span className="font-bold">800K+</span> freelancers and{" "}
+            <span className="font-bold">20k+ teams</span> like.
+          </p>
+        </div>
         <div className="py-10 w-full md:py-20">
           <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden py-10">
             <div className="w-full h-full flex overflow-hidden p-2 flex-row"></div>
@@ -51,7 +57,7 @@ export default function Home() {
       <div className="h-full w-full mx-auto max-w-[1350px] px-4 flex flex-col items-center justify-center py-12 relative gap-10 md:px-20">
         <div className="w-full h-full">
           <div className="max-w-xl mx-auto text-start md:text-center">
-            <span className="inline-flex h-full w-full items-center justify-center rounded-full cursor-pointer bg-slate-950 px-4 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            <span className="h-full w-full items-center justify-center rounded-full cursor-pointer bg-slate-950 px-4 py-1 text-sm font-medium text-white backdrop-blur-3xl border border-blue-800">
               Know who you are
             </span>
             <h2 className="text-3xl font-semibold pt-6 lg:text-4xl">
@@ -65,33 +71,33 @@ export default function Home() {
         </div>
 
         <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
-          <div className="flex flex-col gap-4 bento-card">
+          <div className="flex flex-col gap-4 rounded-2xl p-6 bg-background shadow-[0px_0px_10px_10px_rgba(0,0,0,0.9)]">
             <div>
               <h3 className="text-xl font-semibold">Measure time spent</h3>
               <p className="text-muted-foreground mt-4">
                 Get a deep understanding of where your time goes
               </p>
             </div>
-            <div className="flex flex-1 bg-muted rounded-2xl" />
+            <div className="flex flex-1 bg-gray-800  rounded-2xl" />
           </div>
           <div className="flex flex-col gap-5">
-            <div className="flex flex-col gap-4 flex-1 bento-card">
+            <div className="flex flex-col gap-4 flex-1 rounded-2xl p-6 bg-background shadow-[0px_0px_10px_10px_rgba(0,0,0,0.9)]">
               <div>
                 <h3 className="text-xl font-semibold">Track your habits</h3>
                 <p className="text-muted-foreground mt-4">
                   Keep track of your habits and see how consistent you are
                 </p>
               </div>
-              <div className="flex flex-1 bg-muted rounded-2xl p-20" />
+              <div className="flex flex-1 bg-gray-800  rounded-2xl p-20" />
             </div>
-            <div className="flex flex-col gap-4 flex-1 bento-card">
+            <div className="flex flex-col gap-4 flex-1 rounded-2xl p-6 bg-background shadow-[0px_0px_10px_10px_rgba(0,0,0,0.9)]">
               <div>
                 <h3 className="text-xl font-semibold">Set goals</h3>
                 <p className="text-muted-foreground mt-4">
                   Set goals and track your progress
                 </p>
               </div>
-              <div className="flex flex-1 bg-muted rounded-2xl p-20" />
+              <div className="flex flex-1 bg-gray-800 rounded-2xl p-20" />
             </div>
           </div>
         </div>
@@ -100,7 +106,7 @@ export default function Home() {
       <div className="h-full w-full mx-auto max-w-[1350px] px-4 flex flex-col items-center justify-center py-12 relative gap-10 md:px-20">
         <div className="w-full h-full">
           <div className="max-w-md mx-auto text-start md:text-center">
-            <span className="inline-flex h-full w-full items-center justify-center rounded-full cursor-pointer bg-slate-950 px-4 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            <span className="h-full w-full items-center justify-center border border-blue-800 rounded-full cursor-pointer bg-slate-950 px-4 py-1 text-sm font-medium text-white backdrop-blur-3xl">
               What we offer as price
             </span>
             <h2 className="text-3xl font-semibold pt-6 lg:text-4xl">
@@ -115,7 +121,7 @@ export default function Home() {
 
         <div className="w-full h-full">
           <div className="grid grid-cols-1 gap-5 my-20 mx-auto max-w-3xl md:grid-cols-2">
-            <div className="flex flex-col gap-4 p-8 rounded-xl border border-border bg-background">
+            <div className="flex flex-col gap-4 p-8 rounded-xl borders bg-background">
               <div>
                 <h3 className="text-xl font-semibold">Monthly plan</h3>
                 <p className="text-muted-foreground mt-4">
@@ -132,21 +138,25 @@ export default function Home() {
 
                 <div className="flex flex-col gap-2">
                   <div className="flex flex-row items-centergap-2">
+                    <span>.</span>
                     <span>Full access to all features</span>
                   </div>
                   <div className="flex flex-row items-centergap-2">
+                    <span>.</span>
                     <span>Regular updates and new features</span>
                   </div>
                   <div className="flex flex-row items-centergap-2">
+                    <span>.</span>
                     <span>Premium support</span>
                   </div>
                   <div className="flex flex-row items-centergap-2">
+                    <span>.</span>
                     <span>No recurring fees</span>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="flex flex-col gap-4 p-8 border border-border bg-background rounded-xl">
+            <div className="flex flex-col gap-4 p-8 borders bg-background rounded-xl">
               <div>
                 <h3 className="text-xl font-semibold">Pay once lifetime</h3>
                 <p className="text-muted-foreground mt-4">
@@ -163,15 +173,19 @@ export default function Home() {
 
                 <div className="flex flex-col gap-2">
                   <div className="flex flex-row items-centergap-2">
+                    <span>.</span>
                     <span>Full access to all features</span>
                   </div>
                   <div className="flex flex-row items-centergap-2">
+                    <span>.</span>
                     <span>Regular updates and new features</span>
                   </div>
                   <div className="flex flex-row items-centergap-2">
+                    <span>.</span>
                     <span>Premium support</span>
                   </div>
                   <div className="flex flex-row items-centergap-2">
+                    <span>.</span>
                     <span>No recurring fees</span>
                   </div>
                 </div>
@@ -184,7 +198,7 @@ export default function Home() {
       <div className="h-full w-full mx-auto max-w-[1350px] px-4 flex flex-col items-center justify-center py-12 relative gap-10 md:px-20">
         <div className="w-full h-full">
           <div className="max-w-md mx-auto text-start md:text-center">
-            <span className="inline-flex h-full w-full items-center justify-center rounded-full cursor-pointer bg-slate-950 px-4 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            <span className="h-full w-full items-center justify-center border border-blue-800 rounded-full cursor-pointer bg-slate-950 px-4 py-1 text-sm font-medium text-white backdrop-blur-3xl">
               What they say
             </span>
             <h2 className="text-3xl font-semibold pt-6 lg:text-4xl">
@@ -200,7 +214,7 @@ export default function Home() {
         <div className="w-full py-10 md:py-20">
           <div className="relative h-full w-full flex flex-col items-center justify-center overflow-hidden py-10">
             <div className="group flew overflow-hidden p-2 select-none flex-row [--duration.20s]">
-              <figure className="relative flex-shrink-0 w-64 p-4 border border-border over:lg-zinc-50/[.15] overflow-hidden rounded-xl bg-background cursor-pointer">
+              <figure className="relative flex-shrink-0 w-64 p-4 borders over:lg-zinc-50/[.15] overflow-hidden rounded-xl bg-background cursor-pointer">
                 <div className="flex flex-row items-center gap-2">
                   <div>
                     <User />
@@ -220,7 +234,7 @@ export default function Home() {
               </figure>
             </div>
             <div className="group flew overflow-hidden p-2 select-none flex-row [--duration.20s]">
-              <figure className="relative flex-shrink-0 w-64 p-4 border border-border over:lg-zinc-50/[.15] overflow-hidden rounded-xl bg-background cursor-pointer">
+              <figure className="relative w-64 p-4 borders over:lg-zinc-50/[.15] overflow-hidden rounded-xl bg-background cursor-pointer">
                 <div className="flex flex-row items-center gap-2">
                   <div>
                     <User />
@@ -239,6 +253,102 @@ export default function Home() {
                 </blockquote>
               </figure>
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="h-full w-full mx-auto px-4 my-32 max-w-[1350px] md:px-20">
+        <div className="flex flex-col items-start justify-start  w-full px-4 md:px-6 rounded-lg borders py-4 md:py-8 lg:rounded-2xl md:justify-between md:items-center md:flex-row">
+          <div className="flex flex-col items-start gap-4 w-full max-w-lg">
+            <h4 className="text-xl font-semibold lg:text-4xl md:text-2xl">
+              Join our <span className="text-muted-foreground">newsletter</span>
+            </h4>
+            <p className="text-base text-muted-foreground">
+              Join our newsletter for exclusive insights, announcements, and
+              special offers delivered directly to your inbox.
+            </p>
+          </div>
+          <div className="flex flex-col gap-2 items-start w-full mt-5 md:min-w-80 md:mt-0 md:w-max">
+            <form
+              action=""
+              className="flex flex-col items-center gap-2 w-full md:max-w-xs md:flex-row"
+            >
+              <input
+                type="text"
+                className="h-9 w-full flex rounded-md borders bg-transparent px-3 py-1 text-sm"
+                placeholder="example@gmail.com"
+              />
+              <button className="rounded-md text-white/50 bg-violet-800/50 px-5 py-1.5">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <div className="py-10">
+        <div className="h-full w-full mx-auto px-4 max-w-[1350px] md:px-20">
+          <div className="flex flex-col gap-10 pb-28 border-b border-bottom lg:flex-row">
+            <div className="flex flex-1 gap-5 flex-col">
+              <div className="flex gap-2 items-center text-lg font-medium">
+                <div className="size-8 rounded-md bg-slate-900 transition-all duration-200" />
+                <span>
+                  Todo<span className="text-gray-400">Fusion</span>
+                </span>
+              </div>
+              <p className="text-base text-gray-400">
+                Simplify tasks, boost productivity, and manage projects
+                seamlessly.
+              </p>
+            </div>
+            <div className="flex flex-row flex-1 gap-10">
+              <div className="flex flex-col gap-5 flex-1">
+                <h3>Pages</h3>
+                <ul className="flex flex-col gap-2 text-gray-400">
+                  <li>Home</li>
+                  <li>Features</li>
+                  <li>Integrations</li>
+                  <li>Pricing</li>
+                  <li>Docs</li>
+                  <li>Contacts</li>
+                </ul>
+              </div>
+              <div className="flex-1">
+                <div className="flex flex-col gap-5">
+                  <h3>Pages</h3>
+                  <ul className="flex flex-col gap-2 text-gray-400">
+                    <li>Features</li>
+                    <li>Integrations</li>
+                    <li>Docs</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="flex-1">
+                <div className="flex flex-col gap-5">
+                  <h3>Pages</h3>
+                  <ul className="flex flex-col gap-2 text-gray-400">
+                    <li>Blog</li>
+                    <li>FAQ</li>
+                    <li>FAQ</li>
+                    <li>FAQ</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="py-10 flex flex-col gap-5">
+            <div className="flex items-center justify-between ">
+              <p>© 2024 TodoFusion. All rights reserved.</p>
+              <div className="flex items-center gap-2">
+                <div className="w-6 h-6 rounded-md cursor-pointer bg-slate-900" />
+                <div className="w-6 h-6 rounded-md cursor-pointer bg-slate-900" />
+                <div className="w-6 h-6 rounded-md cursor-pointer bg-slate-900" />
+              </div>
+            </div>
+            <p className="text-sm text-foreground text-center font-medium">
+              Made with 🩶{" "}
+              <a className="duration-200 text-blue-800">by Kwaham Grace.</a>{" "}
+            </p>
           </div>
         </div>
       </div>
